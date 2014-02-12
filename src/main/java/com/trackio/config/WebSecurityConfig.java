1dd901d733f3746a01e6c92896e22a7da08974d0
Jatinder Singh <jsingh@centos.server.com>
2014-02-12 12:49:05 -0500

    Adds security and a templating engine

---
+++ src/main/java/com/trackio/config/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.trackio.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+/**
+ * @author jsingh on 2/12/14.
+ */
+@Configuration
+@EnableWebMvcSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // URLs configured to not require any authentication. All other paths must be authenticated.
+        http
+                .authorizeRequests()
+                    .antMatchers("/", "/home").permitAll()
+                    .anyRequest().authenticated();
+
+        // Defines custom login page (via loginPage(..)) and everyone is allowed to view it
+        http
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                    .and()
+                .logout()
+                    .permitAll();
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                    .withUser("john").password("adminadmin").roles("USER");
+    }
+}
