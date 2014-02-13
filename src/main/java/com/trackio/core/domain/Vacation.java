aea0bc0ce6cf7e78458668a640e0979633247a72
Jatinder Singh <jsingh@centos.server.com>
2014-02-13 13:47:28 -0500

    Creates domain classes

---
+++ src/main/java/com/trackio/core/domain/Vacation.java
@@ -0,0 +1,7 @@
+package com.trackio.core.domain;
+
+/**
+ * @author jsingh on 2/13/14.
+ */
+public class Vacation {
+}
---
+++ src/main/java/com/trackio/core/domain/VacationStatusHistory.java
@@ -0,0 +1,10 @@
+package com.trackio.core.domain;
+
+import java.util.UUID;
+
+/**
+ * @author jsingh on 2/13/14.
+ */
+public class VacationStatusHistory {
+    
+}
