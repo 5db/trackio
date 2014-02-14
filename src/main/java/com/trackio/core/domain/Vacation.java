2edfc809e12a9e029495631ad0d3b433bbd75cb5
Jatinder Singh <bitatomy@gmail.com>
2014-02-13 22:58:14 -0500

    Adds spring data

--- src/main/java/com/trackio/core/domain/Vacation.java
+++ src/main/java/com/trackio/core/domain/Vacation.java
@@ -1,7 +1,31 @@
 package com.trackio.core.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * @author jsingh on 2/13/14.
  */
+@Entity
 public class Vacation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long vacationId;
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getVacationId() {
+        return vacationId;
+    }
 }
